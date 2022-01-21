99aace66469368e1ead29462645e328d0f51d9e3
Dong-eun Shin <55118858+dongeunshin@users.noreply.github.com>
2022-01-21 14:41:45 +0900

    Delete Hash directory

--- Hash/완주하지 못한 선수.py
+++
@@ -1,9 +0,0 @@
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
-    return participant[-1]
-  
-  # sort가 포인트다. sort를 떠올려라!!
