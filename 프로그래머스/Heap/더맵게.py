3f87a23464324d4788d5008340370425658d8838
Dong-eun Shin <55118858+dongeunshin@users.noreply.github.com>
2022-01-24 12:36:20 +0900

    2022/02/24 (#8)
    * Create 주식가격.py
    
    * Create 기능개발.py
    
    * Create 다리를 지나는 트럭.py
    
    * Create 완주하지 못한 선수.py
    
    * Create 전화번호 목록.py
    
    * Create 위장.py
    
    * Create 베스트앨범.py
    
    * Create 베스트앨범.py
    
    * Delete 프로그래머스/Hash directory
    
    * Create 완주하지 못한 선수.py
    
    * Rename Hash/베스트앨범.py to 프로그래머스/Hash/베스트앨범.py
    
    * Rename Hash/위장.py to 프로그래머스/Hash/위장.py
    
    * Rename Hash/전화번호 목록.py to 프로그래머스/Hash/전화번호 목록.py
    
    * Delete Hash directory
    
    * 💬Comment: 22/01/24 프로그래머스 더맵게
    
    * ✒️Feat: 22/01/24 프로그래머스 더맵게

---
+++ 프로그래머스/Heap/더맵게.py
@@ -0,0 +1,15 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    value = 0
+    heapq.heapify(scoville)
+    print(heapq)
+    while scoville[0] < K:
+        value = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
+        heapq.heappush(scoville,value)
+        answer += 1
+        if len(scoville) == 1 and scoville[0] < K:
+            return -1
+    return answer
+  
+  # heap을 직접 구현해서 풀어바야 헐까?? 그래여지..
