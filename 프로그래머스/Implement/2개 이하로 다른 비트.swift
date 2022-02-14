714f52338e6b6ae73f8a845ffb7eba5afd87ca56
Dong-eun Shin <55118858+dongeunshin@users.noreply.github.com>
2022-02-14 17:32:58 +0900

    🛠Fix: 22/02/14 프로그래머스 2개 이하로 다른 비트

--- 프로그래머스/Implement/2개 이하로 다른 비트.swift
+++ 프로그래머스/Implement/2개 이하로 다른 비트.swift
@@ -1,33 +1,22 @@
-import Foundation
+iimport Foundation
 
-func convertTo16(_ b:String) -> String{
-    var binary = b
-    if binary.count != 16 {
-        binary = String(repeating: "0", count: (16 - binary.count)) + binary
-    }
-    return binary
-}
 func solution(_ numbers:[Int64]) -> [Int64] {
     var ans: [Int64] = []
     for num in numbers{
-        var i: Int64 = 1
-        while true{
-            var binary = String(num, radix: 2)
-            var nextB = String(num + i, radix: 2)
-            binary = convertTo16(binary)
-            nextB = convertTo16(nextB)
-            var cnt = 0
-            for index in 0..<binary.count{
-                if binary[binary.index(binary.`startIndex`, offsetBy: index)] != nextB[nextB.index(nextB.startIndex, offsetBy: index)]{
-                    cnt += 1
-                }
-            }
-            if cnt == 1 || cnt == 2{
-                ans.append(Int64(num + i))
-                break
-            }
-            i += 1
+        let b = String(num, radix: 2)
+        var arr = b.map{Int(String($0))!}
+        var i = 0
+        if let index = arr.lastIndex(of: 0) {
+            arr[index] = 1
+            i = index
+        }else {
+            arr.append(1)
         }
+        if i != arr.count - 1{
+            arr[i+1] = 0
+        }
+        let binary = arr.map{String($0)}.joined(separator: "")
+        ans.append(Int64(binary, radix: 2)!)
     }
     return ans
 }
